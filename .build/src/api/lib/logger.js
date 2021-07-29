9a095d2feab10e63d0decad79253da0d8de89ddb
Jesse Mull <jessemull@gmail.com>
2021-07-28 21:27:35 -0700

    Add build dir to gitignore

--- .build/src/api/lib/logger.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const lambda_log_1 = __importDefault(require("lambda-log"));
-exports.default = lambda_log_1.default;
-//# sourceMappingURL=logger.js.map
