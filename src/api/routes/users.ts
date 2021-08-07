deee0ce90e00f4aa11269e3b2f638dec41068857
Jesse Mull <jessemull@gmail.com>
2021-08-06 17:54:24 -0700

    Added update and delete endpoints for users

--- src/api/routes/users.ts
+++ src/api/routes/users.ts
@@ -13,10 +13,15 @@ export default (app: Router): void => {
 
   route.get('/', async (req: Request, res: Response): Promise<void> => {
     try {
+      // Retrieve e-mail from JWT
+
       const authorization = req.headers.authorization || ''
       const split = authorization?.split(' ')
       const jwt: { sub: string } = decodeJWT(split[1])
       const email = jwt.sub
+
+      // Get the dynamo user
+
       const params = {
         TableName: USERS_TABLE,
         Key: {
@@ -24,6 +29,9 @@ export default (app: Router): void => {
         }
       }
       const data = await dynamoDb.get(params).promise()
+
+      // Check if user is found and return user details
+
       if (data.Item) {
         const { id, email, firstName, lastName, login } = data.Item as User
         res.json({ id, email, firstName, lastName, login })
@@ -38,6 +46,8 @@ export default (app: Router): void => {
 
   route.post('/', async (req: Request, res: Response): Promise<void> => {
     try {
+      // Create OKTA user
+
       const oktaClient = await getOktaClient()
       const { email, firstName, lastName, password } = req.body as User
       const newUser = {
@@ -54,6 +64,9 @@ export default (app: Router): void => {
         }
       }
       const user = await oktaClient.createUser(newUser)
+
+      // Create dynamo user
+
       const params = {
         TableName: USERS_TABLE,
         Item: {
@@ -66,6 +79,9 @@ export default (app: Router): void => {
         ReturnValues: 'ALL_OLD'
       }
       await dynamoDb.put(params).promise()
+
+      // Return OKTA user
+
       res.json(user)
     } catch (error) {
       logger.error(error)
@@ -73,25 +89,81 @@ export default (app: Router): void => {
     }
   })
 
-  route.put('/:id', async (req: Request, res: Response): Promise<void> => {
-    const { email, firstName, lastName, login } = req.body as User
-    const params = {
-      TableName: USERS_TABLE,
-      Item: {
-        id: req.params.id,
-        email,
-        firstName,
-        lastName,
-        login
-      },
-      ReturnValues: 'ALL_OLD'
-    }
+  route.put('/', async (req: Request, res: Response): Promise<void> => {
     try {
-      const user = await dynamoDb.put(params).promise()
-      res.json(user.Attributes)
+      // Retrieve e-mail from JWT
+
+      const authorization = req.headers.authorization || ''
+      const split = authorization?.split(' ')
+      const jwt: { sub: string } = decodeJWT(split[1])
+      const email = jwt.sub
+      const { firstName, lastName } = req.body as User
+
+      // Update OKTA user
+
+      const oktaClient = await getOktaClient()
+      const user = await oktaClient.getUser(email)
+      user.profile.firstName = firstName
+      user.profile.lastName = lastName
+      await user.update()
+
+      // Update dynamo user
+
+      const params = {
+        TableName: USERS_TABLE,
+        Key: {
+          email
+        },
+        UpdateExpression: 'set firstName = :f, lastName = :l',
+        ExpressionAttributeValues: {
+          ':f': firstName,
+          ':l': lastName
+        },
+        ReturnValues: 'ALL_OLD'
+      }
+      await dynamoDb.update(params).promise()
+
+      // Return OKTA user
+
+      res.json(user)
     } catch (error) {
       logger.error(error)
       res.status(500).json({ error: 'Could not update user' })
+    }
+  })
+
+  route.delete('/', async (req: Request, res: Response): Promise<void> => {
+    try {
+      // Retrieve e-mail from JWT
+
+      const authorization = req.headers.authorization || ''
+      const split = authorization?.split(' ')
+      const jwt: { sub: string } = decodeJWT(split[1])
+      const email = jwt.sub
+
+      // Delete the user from OKTA
+
+      const oktaClient = await getOktaClient()
+      const user = await oktaClient.getUser(email)
+      await user.deactivate()
+      await user.delete()
+
+      // Delete the user from dynamo
+
+      const params = {
+        TableName: USERS_TABLE,
+        Key: {
+          email
+        }
+      }
+      await dynamoDb.delete(params).promise()
+
+      // Return 204 for successful delete
+
+      res.status(204).send()
+    } catch (error) {
+      logger.error(error)
+      res.status(500).json({ error: 'Could not delete user' })
     }
   })
 }
